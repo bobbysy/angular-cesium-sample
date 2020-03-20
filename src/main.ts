158c623d1b633dc2e02bd10f9b0915e7053be45d
bobbysy <25177772+bobbysy@users.noreply.github.com>
2020-03-20 23:47:07 +0800

    chore: Format codes

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core";
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module";
+import { environment } from "./environments/environment";
 
 // If youre using Cesium version >= 1.42.0 add this line
-Cesium.buildModuleUrl.setBaseUrl('/assets/cesium/');
-Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTY1NDYzYS01YzgxLT'
-  + 'Q2MGUtODBiYy0zODRmY2MwOGY4MDIiLCJpZCI6MjA1LCJpYXQiOjE1MDQ3MjQ1Njh9.rKgXUKAfFiiSAm_b9T8bpsDVdj0YyZeqGxNpzLlhxpk';
+Cesium.buildModuleUrl.setBaseUrl("/assets/cesium/");
+Cesium.Ion.defaultAccessToken =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI2YTY1NDYzYS01YzgxLT" +
+  "Q2MGUtODBiYy0zODRmY2MwOGY4MDIiLCJpZCI6MjA1LCJpYXQiOjE1MDQ3MjQ1Njh9.rKgXUKAfFiiSAm_b9T8bpsDVdj0YyZeqGxNpzLlhxpk";
 if (environment.production) {
   enableProdMode();
-  Cesium.buildModuleUrl.setBaseUrl('/angular-cesium-sample/assets/cesium/');
+  Cesium.buildModuleUrl.setBaseUrl("/angular-cesium-sample/assets/cesium/");
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
